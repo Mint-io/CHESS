80b3a607243c81ec3fc4c024b90842f0c0487b67
YIMING DAI <ilime.master@gmail.com>
2019-07-27 12:41:44 -0400

    Reconstruct piece.h/cc

--- piece.h
+++ piece.h
@@ -1,24 +1,43 @@
 #ifndef _PIECE_H_
 #define _PIECE_H_
 #include <vector>
+#include <iostream>
+#include "cell.h"
 
 class Cell;
-
 class Board;
 
 class Piece {
-    char side; // 'b' for black, 'w' for white
+
+protected:
+    char chessType; // QKBNRP or qkbnrp
+    bool isAlive; //whether removed or not
     int x; // x axis
+    int x_prev; // x_prev axis
     int y; // y axis
-    protected:
+    int y_prev; // y_prev axis
     std::shared_ptr<Board> gameboard;
     std::vector<Cell *> movelist;
     std::vector<Cell *> attacklist;
     std::vector<Cell> historylist;
-    public:
-    char getSide();
+public:
+    Piece(bool isAlive, int x, int y, std::shared_ptr<Board> gameboard, char chessType) :
+        isAlive{isAlive}, x{x}, y{y}, gameboard{gameboard}, chessType{chessType} {
+        x_prev = x;
+        y_prev = y;
+    }
+    char getType();
+    void setX(int x);
+    void setY(int y);
+    void setX_prev(int x);
+    void setY_prev(int y);
     int getX();
     int getY();
+    int getX_prev();
+    int getY_prev();
+    std::vector<Cell *> &getMoveList();
+    std::vector<Cell *> &getAttackList();
+    bool alive();
 
     // execute a valid move to a cell that contained in movelist or attacklist
     virtual void moveto(int x, int y);
@@ -43,6 +62,9 @@ class Piece {
     // castling move
     // parameter is one of the four corner of the gameboard
     virtual bool validcastling(Cell);
+
+    // see if cell blocks the way
+    virtual bool blockCell(int r, int c) = 0;
 };
 
 
